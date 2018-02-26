fadb25c77b1cc8735c4a18f29f4b33beace66c2f
robert bell <rabell3@gmail.com>
2018-02-25 20:43:05 -0500

    Moved findourstats function to learn, where it belongs.

--- mod1/BH/Tools/learn.py
+++ mod1/BH/Tools/learn.py
@@ -2,6 +2,23 @@ import sys, re, itertools, pickle, random, pprint
 from collections import Counter
 from cpeglibs import *
 
+def findourstats(codedmessage):
+    codefreq = findfreq(codedmessage)
+    normfreq = normal_freqs
+
+    for key, value in sorted(normal_freqs.iteritems(), key=lambda (k,v): (v,k),reverse=True):
+        print "%s: %s" % (key, value)
+
+    for key, value in sorted(codefreq.iteritems(), key=lambda (k,v): (v,k),reverse=True):
+        print "%s: %s" % (key, value)
+
+    normfreq=sorted(normfreq.iteritems(), key=lambda (k,v): (v,k),reverse=True)
+    codefreq=sorted(codefreq.iteritems(), key=lambda (k,v): (v,k),reverse=True)
+
+    print normfreq
+    print codefreq
+    return
+
 # -----------------------------------------------------------------------------------------------------
 engsrc = urllib2.urlopen('https://raw.githubusercontent.com/first20hours/google-10000-english/master/google-10000-english-usa.txt')
 fh=sys.argv[1]
@@ -11,6 +28,7 @@ listchars=[]
 print codedmessage
 print fitsenglish(codedmessage,engsrc)
 newchar=""
+#findourstats(codedmessage)
 
 newmesg = codedmessage
 
--- mod1/BH/Tools/subst2.py
+++ mod1/BH/Tools/subst2.py
@@ -2,23 +2,6 @@ import sys, re, itertools, pickle, random, pprint, os
 from collections import Counter,OrderedDict
 from cpeglibs import *
 
-def findourstats(codedmessage):
-    codefreq = findfreq(codedmessage)
-    normfreq = normal_freqs
-
-    for key, value in sorted(normal_freqs.iteritems(), key=lambda (k,v): (v,k),reverse=True):
-        print "%s: %s" % (key, value)
-
-    for key, value in sorted(codefreq.iteritems(), key=lambda (k,v): (v,k),reverse=True):
-        print "%s: %s" % (key, value)
-
-    normfreq=sorted(normfreq.iteritems(), key=lambda (k,v): (v,k),reverse=True)
-    codefreq=sorted(codefreq.iteritems(), key=lambda (k,v): (v,k),reverse=True)
-
-    print normfreq
-    print codefreq
-    return
-
 def seedkey(message):
     normfreq = normal_freqs
     codefreq = findfreq(codedmessage)
@@ -45,28 +28,12 @@ engsrc = urllib2.urlopen('https://raw.githubusercontent.com/first20hours/google-
 fh=sys.argv[1]
 codedmessage=readfile(fh)
 
-normfreq = normal_freqs
-codefreq = findfreq(codedmessage)
+#findourstats(codedmessage)
+thiskey = seedkey(codedmessage)
 
-normfreq=sorted(normfreq.iteritems(), key=lambda (k,v): (v,k),reverse=True)
-codefreq=sorted(codefreq.iteritems(), key=lambda (k,v): (v,k),reverse=True)
-
-findourstats(codedmessage)
-
-letters = range(26)
-random.shuffle(letters)
-
-print codedmessage
+print "encoded: " , codedmessage
 
 decoded = codedmessage
-#for i in range(26):
-#    oldc, cstat = codefreq[i]
-#    newc, nstat = normfreq[i]
-#    print oldc , " -> " , newc
-#    decoded =  swap_chars(decoded, oldc, newc)
-#    print decoded
 
-#print "coded: " , codedmessage
-
-print seedkey(codedmessage)
+print "seedkey: " , seedkey(codedmessage)
 print "decoded: " , decoded
